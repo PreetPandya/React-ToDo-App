60fe19aaaee2145a3b735d42dfa6b842a13f29fa
Preet Pandya <68130119+PreetPandya@users.noreply.github.com>
2024-02-23 12:35:57 +0530

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -7,13 +7,11 @@ export default function TaskList(props) {
         <div className="tasks-list">
           <span className="input-task">{props.item}</span>
         </div>
-        <div className="icons-list">
-          <span className="icons">
-            <i
-              className="fa-solid fa-trash-can icon-delete"
-              onClick={(e) => props.delete(props.index)}
-            ></i>
-          </span>
+        <div className="icons">
+          <i
+            className="fa-solid fa-trash-can icon-delete"
+            onClick={(e) => props.delete(props.index)}
+          ></i>
         </div>
       </li>
     </div>
